f6456ec7a10a48e7b25193609ee309b7e83b1e87
MatheusViniciusDosSantos <matheus.vinicius77@hotmail.com>
2023-01-04 10:12:47 -0300

    Feitos os CRUDs iniciais

--- src/main/java/br/com/sta/StaApplication.java
+++ src/main/java/br/com/sta/StaApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SuperlojaApplication {
+public class StaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SuperlojaApplication.class, args);
+		SpringApplication.run(StaApplication.class, args);
 	}
 
 }
